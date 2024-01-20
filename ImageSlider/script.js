061e41eba2a7ec771229814d8aebc799c1f913c0
MirFaizan01 <faizanmeer322@gmail.com>
2024-01-20 23:45:13 +0530

    added the image slider project

---
+++ ImageSlider/script.js
@@ -0,0 +1,25 @@
+let currentIndex = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+function changeSlide(direction) {
+    currentIndex += direction;
+
+    if (currentIndex < 0) {
+        currentIndex = totalSlides - 1;
+    } else if (currentIndex >= totalSlides) {
+        currentIndex = 0;
+    }
+
+    updateSlider();
+}
+
+function updateSlider() {
+    const newTransformValue = -currentIndex * 100 + '%';
+    document.querySelector('.slider').style.transform = 'translateX(' + newTransformValue + ')';
+}
+
+// Automatic slide change every 3 seconds
+setInterval(() => {
+    changeSlide(1);
+}, 3000);
